1896cd5c1a72d5332161e8378f09a483cd66a8b7
Apex <mrcat19823@gmail.com>
2026-06-23 23:58:08 +0400

    main.cpp updated

---
+++ src/build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/mrcat/Coding/Raylib-Network-Framework/game"
+  "/home/mrcat/Coding/Raylib-Network-Framework/game.pdb"
+  "CMakeFiles/game.dir/link.d"
+  "CMakeFiles/game.dir/main.cpp.o"
+  "CMakeFiles/game.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,32 @@
-#pragma once
-#include <cstdio>
-#include <string>
-#define RAYGUI_IMPLEMENTATION
-#include "inc/globs.hpp"
-#include <cstdlib>
-#include <ctime>
-#include <raygui.h>
-#include <enet/enet.h>
+#include "raylib.h"
+#include <cstdint>
 
-Color BlackCellColor = BROWN;
-Color WhiteCellColour = WHITE;
+struct Screen {
+  const char *title;
+  int width, height;
 
-inline void Draw(void);
-inline void ResetGame(void);
-inline void DrawIndexs(void);
-Cell grid[COLS][ROWS];
+  Screen(const char *_title, int _width, int _height)
+      : title(_title), width(_width), height(_height) {}
 
-int main(int argc, char ** argv) {
-    srand(time(0));
+  void Init() { InitWindow(this->width, this->height, this->title); }
+  void Close() { CloseWindow(); };
 
-    InitWindow(screenWidth, screenHeight, "Raylib ChessGame");
-    InitAudioDevice();
-    SetTargetFPS(60);
-    if (enet_initialize() != 0) {
-        fprintf(stderr, "An error occured while intializing Enet\n");
-        return EXIT_FAILURE;
-    }
-    ResetGame();
-    /// Every Frame
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        Draw();
-        EndDrawing();
-    }
-    CloseAudioDevice();
-    CloseWindow();
-    enet_deinitialize();
-    return 0;
+  bool IsOpen() { return !WindowShouldClose(); }
+};
+
+Screen *screen = new Screen("Example", 1000, 800);
+
+int32_t main(int32_t argc, char **argv) {
+  screen->Init();
+
+  while (screen->IsOpen()) {
+    BeginDrawing();
+
+    ClearBackground(DARKBLUE);
+
+    EndDrawing();
+  }
+
+  screen->Close();
+  return 0;
 }
-
-inline void Draw(void) {
-    ClearBackground(GREEN);
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            grid[i][j].DrawCell();
-        }
-    }
-    DrawIndexs();
-}
-
-inline void initGrid(void) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            Color curColor = ((i + j) & 1) ? BlackCellColor : WhiteCellColour;
-            grid[i][j] = (Cell) {.i = i, .j = j, .piece = "empty", .isFree = true, .color = curColor};
-        }
-    }
-}
-
-inline void DrawIndexs(void) {
-    for (int i = 0; i < ROWS; i++) {
-        std::string curchar = "a";
-        curchar[0] += i;
-        int posX = cellHeight * i + cellHeight / 2 + padding - 14;
-        int posY = boardHeight + padding + 10;
-        DrawText(curchar.c_str(),  posX,  posY,  28, RED);
-    }
-    for (int j = 0; j < COLS; j++) {
-        std::string curchar = "8";
-        curchar[0] -= j;
-        int posX = padding - 14 - 10;
-        int posY = cellWidth * j + cellWidth / 2 + padding - 14;
-        DrawText(curchar.c_str(),  posX,  posY,  28, RED);
-    }
-}
-
-inline void ResetGame(void) {
-    initGrid();
-}
-
